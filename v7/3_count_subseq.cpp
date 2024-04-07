ef8f2932c19ec2c534355babb38cd5be138e725f
h4-harshh <harshnew2204@gmail.com>
2024-04-07 12:19:10 +0530

    total subseq whose sum equals to k

--- v7/3_count_subseq.cpp
+++ v7/3_count_subseq.cpp
@@ -21,28 +21,47 @@ using namespace std;
 //     return l + r;
 // }
 
-int find_subsequences(int index, vector<int> &arr, int k, int sum)
+// int find_subsequences(int index, vector<int> &arr, int k, int sum)
+// {
+//     if (index >= arr.size())
+//     {
+//         if (sum == k)
+//         {
+//             return 1;
+//         }
+//         else
+//         {
+//             return 0;
+//         }
+//     }
+//     int l = find_subsequences(index + 1, arr, k, sum);
+
+//     sum += arr[index];
+//     int r = find_subsequences(index + 1, arr, k, sum);
+//     sum -= arr[index];
+
+//     return l + r;
+// }
+
+int find_subsequences(int index, vector<int> &arr, int k, int curr_sum)
 {
     if (index >= arr.size())
     {
-        if (sum == k)
+        if (k == curr_sum)
         {
             return 1;
         }
-        else
-        {
-            return 0;
-        }
+        return 0;
     }
-    int l = find_subsequences(index + 1, arr, k, sum);
 
-    sum += arr[index];
-    int r = find_subsequences(index + 1, arr, k, sum);
-    sum -= arr[index];
+    int left = find_subsequences(index + 1, arr, k, curr_sum);
 
-    return l + r;
+    // res.push_back(arr[index]);
+    int right = find_subsequences(index + 1, arr, k, curr_sum + arr[index]);
+    // res.pop_back();
+
+    return (left + right);
 }
-
 int main()
 {
     vector<int> arr = {1, 2, 1};
