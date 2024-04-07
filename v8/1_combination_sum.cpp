6f2b42e4155cc333c1db708a3f1bdb6d3667cd47
h4-harshh <harshnew2204@gmail.com>
2024-04-08 00:37:42 +0530

    combination sum

--- v8/1_combination_sum.cpp
+++ v8/1_combination_sum.cpp
@@ -1,10 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void combination_sum(vector<int> v, int target, vector<int> &res, int index)
-{
+// void combination_sum(vector<int> v, int target, vector<int> &res, int index)
+// {
 
-    if (index == v.size())
+//     if (index == v.size())
+//     {
+//         if (target == 0)
+//         {
+//             for (auto x : res)
+//             {
+//                 cout << x << " ";
+//             }
+//             cout << endl;
+//         }
+//         return;
+//     }
+//     // function call without picking
+//     combination_sum(v, target, res, index + 1);
+
+//     // function call with picking
+//     if (v[index] <= target)
+//     {
+//         res.push_back(v[index]);
+//         target -= v[index];
+//         combination_sum(v, target, res, index);
+//         target += v[index];
+//         res.pop_back();
+//     }
+// }
+
+void combination_sum(vector<int> &v, int target, vector<int> &res, int curr_index)
+{
+    if (curr_index >= v.size())
     {
         if (target == 0)
         {
@@ -16,18 +44,14 @@ void combination_sum(vector<int> v, int target, vector<int> &res, int index)
         }
         return;
     }
-    // function call without picking
-    combination_sum(v, target, res, index + 1);
 
-    // function call with picking
-    if (v[index] <= target)
+    if (v[curr_index] <= target)
     {
-        res.push_back(v[index]);
-        target -= v[index];
-        combination_sum(v, target, res, index);
-        target += v[index];
+        res.push_back(v[curr_index]);
+        combination_sum(v, target - v[curr_index], res, curr_index);
         res.pop_back();
     }
+    combination_sum(v, target, res, curr_index + 1);
 }
 int main()
 {
