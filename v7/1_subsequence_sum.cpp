4ab43b7a74aee020c877c3a538ad8dca4e391f9a
h4-harshh <harshnew2204@gmail.com>
2024-04-07 12:20:06 +0530

    sub whose sum equals to k

--- v7/1_subsequence_sum.cpp
+++ v7/1_subsequence_sum.cpp
@@ -2,30 +2,6 @@
 // #include <bits/stdc++.h>
 // using namespace std;
 
-// // void find_subsequences(int idx, vector<int> &arr, vector<int> &res, int k, int sum)
-// // {
-// //     if (idx >= arr.size())
-// //     {
-// //         if (sum == k)
-// //         {
-// //             for (auto x : res)
-// //             {
-// //                 cout << x << " ";
-// //             }
-// //             cout << endl;
-// //         }
-// //         return;
-// //     }
-
-// //     find_subsequences(idx + 1, arr, res, k, sum);
-
-// //     res.push_back(arr[idx]);
-// //     sum += arr[idx];
-// //     find_subsequences(idx + 1, arr, res, k, sum);
-// //     sum -= arr[idx];
-// //     res.pop_back();
-// // }
-
 // void find_subsequences(int index, vector<int> &arr, vector<int> &res, int k, int curr_sum)
 // {
 //     if (index >= arr.size())
