52072819736be0721d50eb65742d18b17e369ab3
h4-harshh <harshnew2204@gmail.com>
2024-04-07 11:17:44 +0530

    palindrome revised

--- v4/v4_check_palindrome.cpp
+++ v4/v4_check_palindrome.cpp
@@ -1,32 +1,47 @@
+// palindrom check using 2 pointer
 #include <bits/stdc++.h>
 using namespace std;
 
-// bool check_palin(string str, int i)
-// {
-//     if (i >= (str.length() / 2))
-//         return true;
-
-//     if (str[i] != str[str.length() - 1 - i])
-//         return false;
-
-//     check_palin(str, i + 1);
-// }
-
-bool check_palin(string str, int l, int h)
+bool check_palin(string str, int index)
 {
-    if (l >= h)
+    if (index >= str.length() / 2)
         return true;
 
-    if (str[l] != str[h])
+    if (str[index] != str[str.length() - 1 - index])
         return false;
-    return check_palin(str, l + 1, h - 1);
+
+    return check_palin(str, index + 1);
 }
 
 int main()
 {
-    string str = "harsh";
-    // string str = "nitin";
+    // string str = "harsh";
+    string str = "nitin";
     // cout << boolalpha << check_palin(str, 0) << endl;
-    cout << boolalpha << check_palin(str, 0, str.length() - 1) << endl;
+    cout << boolalpha << check_palin(str, 0) << endl;
     return 0;
 }
+
+// // palindrom check using 1 pointer
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool check_palin(string str, int l, int r)
+// {
+//     if (l >= r)
+//         return true;
+
+//     if (str[l] != str[r])
+//         return false;
+
+//     return check_palin(str, l + 1, r - 1);
+// }
+
+// int main()
+// {
+//     string str = "harsh";
+//     // string str = "nitin";
+//     // cout << boolalpha << check_palin(str, 0) << endl;
+//     cout << boolalpha << check_palin(str, 0, str.length() - 1) << endl;
+//     return 0;
+// }
