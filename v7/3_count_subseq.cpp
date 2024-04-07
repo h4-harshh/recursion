fd2eb2821f6d8efc467d596f3f8c87521c4bc7d0
h4-harshh <harshnew2204@gmail.com>
2024-04-07 12:20:22 +0530

    total no. of sub whose sum equals to k

--- v7/3_count_subseq.cpp
+++ v7/3_count_subseq.cpp
@@ -1,48 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// int find_subsequences(int idx, vector<int> &arr, int k, int sum)
-// {
-//     if (idx >= arr.size())
-//     {
-//         if (sum == k)
-//         {
-//             return 1;
-//         }
-//         return 0;
-//     }
-
-//     int l = find_subsequences(idx + 1, arr, k, sum);
-
-//     sum += arr[idx];
-//     int r = find_subsequences(idx + 1, arr, k, sum);
-//     sum -= arr[idx];
-
-//     return l + r;
-// }
-
-// int find_subsequences(int index, vector<int> &arr, int k, int sum)
-// {
-//     if (index >= arr.size())
-//     {
-//         if (sum == k)
-//         {
-//             return 1;
-//         }
-//         else
-//         {
-//             return 0;
-//         }
-//     }
-//     int l = find_subsequences(index + 1, arr, k, sum);
-
-//     sum += arr[index];
-//     int r = find_subsequences(index + 1, arr, k, sum);
-//     sum -= arr[index];
-
-//     return l + r;
-// }
-
 int find_subsequences(int index, vector<int> &arr, int k, int curr_sum)
 {
     if (index >= arr.size())
