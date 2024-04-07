bee91cfde2433a5765a2b463dc77530ab7b2cfef
h4-harshh <harshnew2204@gmail.com>
2024-04-07 12:18:43 +0530

    subsequences sum equals to k any one

--- v7/2_only_one.cpp
+++ v7/2_only_one.cpp
@@ -1,8 +1,37 @@
-// follow up of previous we have to print only one  subsequence this time
-// this approach is used for all places where we want to get only one subsequence
+// // follow up of previous we have to print only one  subsequence this time
+// // this approach is used for all places where we want to get only one subsequence
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // bool find_subsequences(int idx, vector<int> &arr, vector<int> &res, int k, int sum)
+// // {
+// //     if (idx >= arr.size())
+// //     {
+// //         if (sum == k)
+// //         {
+// //             for (auto x : res)
+// //             {
+// //                 cout << x << " ";
+// //             }
+// //             cout << endl;
+// //             return true;
+// //         }
+// //         return false;
+// //     }
+
+// //     if (find_subsequences(idx + 1, arr, res, k, sum))
+// //         return true;
+
+// //     res.push_back(arr[idx]);
+// //     sum += arr[idx];
+// //     if (find_subsequences(idx + 1, arr, res, k, sum))
+// //         return true;
+// //     sum -= arr[idx];
+// //     res.pop_back();
+
+// //     return false;
+// // }
 
 // bool find_subsequences(int idx, vector<int> &arr, vector<int> &res, int k, int sum)
 // {
@@ -21,47 +50,56 @@ using namespace std;
 //     }
 
 //     if (find_subsequences(idx + 1, arr, res, k, sum))
+//     {
 //         return true;
+//     };
 
 //     res.push_back(arr[idx]);
 //     sum += arr[idx];
 //     if (find_subsequences(idx + 1, arr, res, k, sum))
+//     {
 //         return true;
+//     };
 //     sum -= arr[idx];
 //     res.pop_back();
 
 //     return false;
 // }
 
-bool find_subsequences(int idx, vector<int> &arr, vector<int> &res, int k, int sum)
+// int main()
+// {
+//     vector<int> arr = {1, 2, 1};
+//     int k = 2;
+//     vector<int> res;
+//     int index = 0;
+//     find_subsequences(index, arr, res, k, 0);
+//     return 0;
+// }
+
+// follow up of previous we have to print only one  subsequence this time
+// this approach is used for all places where we want to get only one subsequence
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool find_subsequences(int index, vector<int> &arr, vector<int> &res, int k, int curr_sum)
 {
-    if (idx >= arr.size())
+    if (index >= arr.size())
     {
-        if (sum == k)
+        if (k == curr_sum)
         {
-            for (auto x : res)
-            {
-                cout << x << " ";
-            }
-            cout << endl;
             return true;
         }
         return false;
     }
 
-    if (find_subsequences(idx + 1, arr, res, k, sum))
-    {
+    res.push_back(arr[index]);
+    if (find_subsequences(index + 1, arr, res, k, curr_sum + arr[index]))
         return true;
-    };
-
-    res.push_back(arr[idx]);
-    sum += arr[idx];
-    if (find_subsequences(idx + 1, arr, res, k, sum))
-    {
-        return true;
-    };
-    sum -= arr[idx];
     res.pop_back();
+
+    if (find_subsequences(index + 1, arr, res, k, curr_sum))
+        return true;
 
     return false;
 }
@@ -72,6 +110,12 @@ int main()
     int k = 2;
     vector<int> res;
     int index = 0;
-    find_subsequences(index, arr, res, k, 0);
+    if (find_subsequences(index, arr, res, k, 0))
+    {
+        for (auto x : res)
+        {
+            cout << x << " ";
+        }
+    }
     return 0;
 }
